2197da3e033014df1672b93601b961d5affa1557
Freeda Moore <freedamoore@outlook.com>
2020-04-22 15:00:25 +1000

    Updates

---
+++ precache-manifest.e3df629fb9b54d47b3de509696e9cead.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c6bd64443790c2c727e6a717b6e6e244",
+    "url": "/covid-19-tracker/index.html"
+  },
+  {
+    "revision": "7eb2227876f300ab17ac",
+    "url": "/covid-19-tracker/static/css/main.8205ff0a.chunk.css"
+  },
+  {
+    "revision": "f50bbf3ffc0b299b891f",
+    "url": "/covid-19-tracker/static/js/2.4f76c592.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/covid-19-tracker/static/js/2.4f76c592.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7eb2227876f300ab17ac",
+    "url": "/covid-19-tracker/static/js/main.c0b72f3f.chunk.js"
+  },
+  {
+    "revision": "3d1c3a839b5135420af2",
+    "url": "/covid-19-tracker/static/js/runtime-main.94eb6b52.js"
+  }
+]);
