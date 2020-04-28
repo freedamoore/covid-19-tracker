c7b8f15edeb4a0f0965bd17cdd93514fda8d1bb4
Freeda Moore <freedamoore@outlook.com>
2020-04-28 10:21:50 +1000

    Updates

--- precache-manifest.46a8d036b697c9dfb2987b79f3021248.js
+++ precache-manifest.46a8d036b697c9dfb2987b79f3021248.js
@@ -1,11 +1,11 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "d8a0287192f7eef9fee71dffac9216ed",
+    "revision": "9b1fd4231ecbfd192632203fce4ff6e9",
     "url": "/covid-19-tracker/index.html"
   },
   {
-    "revision": "7c8f1cfe6ad80ba3d41a",
-    "url": "/covid-19-tracker/static/css/main.fbcb926f.chunk.css"
+    "revision": "616e62a223fdb3f3cab0",
+    "url": "/covid-19-tracker/static/css/main.1ee93de0.chunk.css"
   },
   {
     "revision": "6be93917f60d353091f0",
@@ -16,7 +16,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/covid-19-tracker/static/js/2.062a6bc6.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "7c8f1cfe6ad80ba3d41a",
+    "revision": "616e62a223fdb3f3cab0",
     "url": "/covid-19-tracker/static/js/main.34a90925.chunk.js"
   },
   {
