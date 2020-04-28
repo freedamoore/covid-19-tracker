9d1a5515008439ce420d67dc1929ce11af885525
Freeda Moore <freedamoore@outlook.com>
2020-04-28 10:35:07 +1000

    Updates

--- precache-manifest.919895dfdde9808d0e8cebfb9916247a.js
+++ precache-manifest.919895dfdde9808d0e8cebfb9916247a.js
@@ -1,11 +1,11 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "4dde50652f35d3004975bb5c88d10252",
+    "revision": "323e9fa67ce328e6cb762c1baf64d470",
     "url": "/covid-19-tracker/index.html"
   },
   {
-    "revision": "b50f71170cd33c60f25b",
-    "url": "/covid-19-tracker/static/css/main.56b15d09.chunk.css"
+    "revision": "802c1a4416f6f58ef4ee",
+    "url": "/covid-19-tracker/static/css/main.68268fbb.chunk.css"
   },
   {
     "revision": "6be93917f60d353091f0",
@@ -16,7 +16,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/covid-19-tracker/static/js/2.062a6bc6.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "b50f71170cd33c60f25b",
+    "revision": "802c1a4416f6f58ef4ee",
     "url": "/covid-19-tracker/static/js/main.34a90925.chunk.js"
   },
   {
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/covid-19-tracker/precache-manifest.0c8190264e376a31ffc33112cd1d2f02.js"
+  "/covid-19-tracker/precache-manifest.919895dfdde9808d0e8cebfb9916247a.js"
 );
 
 self.addEventListener('message', (event) => {
